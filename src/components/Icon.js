629f8da636657abcd4e07c0ccb8a7339c0ef131c
Philippe Louis B. Garibay <louisgaribay7000@gmail.com>
2025-02-20 14:41:45 +0800

    added all types in our switch statement

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,7 +1,25 @@
 import { StyleSheet, Image, TouchableWithoutFeedback } from 'react-native';
 import React from 'react';
-import Grass from '../assets/Icons/GrassIcon.png'; // ✅ Fixed import path
-import Poison from '../assets/Icons/PoisonIcon.png'; // ✅ Ensure default case works
+import Grass from '../assets/Icons/GrassIcon.png';
+import Poison from '../assets/Icons/PoisonIcon.png';
+import Dark from '../assets/Icons/Darkicon.png';
+import Dragon from '../assets/Icons/DragonIcon.png';
+import Electric from '../assets/Icons/ElectricIcon.png'; // ✅ Fixed space issue in filename
+import Fairy from '../assets/Icons/FairyIcon.png';
+import Fighting from '../assets/Icons/FightingIcon.png';
+import Fire from '../assets/Icons/FireIcon.png';
+import Flying from '../assets/Icons/FlyingIcon.png'; // ✅ Fixed
+import Ghost from '../assets/Icons/GhostIcon.png'; // ✅ Fixed
+import Ground from '../assets/Icons/GroundIcon.png'; // ✅ Fixed
+import Ice from '../assets/Icons/IceIcon.png'; // ✅ Fixed
+import Normal from '../assets/Icons/NormalIcon.png'; // ✅ Fixed
+import Psychic from '../assets/Icons/PsychicIcon.png'; // ✅ Fixed
+import Rock from '../assets/Icons/RockIcon.png'; // ✅ Fixed
+import Steel from '../assets/Icons/SteelIcon.png'; // ✅ Fixed
+import Water from '../assets/Icons/WaterIcon.png'; // ✅ Fixed
+
+
+
 
 const Icon = ({ children, onPress, name, style, ...props }) => {
   let RenderIcon;
@@ -10,9 +28,58 @@ const Icon = ({ children, onPress, name, style, ...props }) => {
     case 'grass':
       RenderIcon = Grass;
       break;
-    default:
+    case 'poison':
       RenderIcon = Poison;
+      break;
+    case 'dark':
+      RenderIcon = Dark;
+      break;
+    case 'dragon':
+      RenderIcon = Dragon;
+      break;
+    case 'electric':
+      RenderIcon = Electric;
+      break;
+    case 'fairy':
+      RenderIcon = Fairy;
+      break;
+    case 'fighting':
+      RenderIcon = Fighting;
+      break;
+    case 'fire':
+      RenderIcon = Fire;
+      break;
+    case 'flying':
+      RenderIcon = Flying;
+      break;
+    case 'ghost':
+      RenderIcon = Ghost;
+      break;
+    case 'ground':
+      RenderIcon = Ground;
+      break;
+    case 'ice':
+      RenderIcon = Ice;
+      break;
+    case 'normal':
+      RenderIcon = Normal;
+      break;
+    case 'psychic':
+      RenderIcon = Psychic;
+      break;
+    case 'rock':
+      RenderIcon = Rock;
+      break;
+    case 'steel':
+      RenderIcon = Steel;
+      break;
+    case 'water':
+      RenderIcon = Water;
+      break;
+    default:
+      RenderIcon = Poison; // Default to Poison if no match is found
   }
+  
 
   return (
     <TouchableWithoutFeedback onPress={onPress}>
